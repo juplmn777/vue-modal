7f0693273fc9474f4a3752b4668c9eead502ec2c
juplmn777 <julien.plumyoen@gmail.com>
2021-12-08 16:40:13 +0100

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,114 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ mainTitle }}</h1>
+  <p>{{ text }}</p>
+  <div v-if="showModal">
+    <TheModal @close="toggleModal">
+      <template v-slot:links>
+        <a href="#">sign up now</a>
+        <a href="#">More infos</a>
+      </template>
+      <h2>Hello from Vue 3</h2>
+      <p>Learning Vue seems cool !</p>
+    </TheModal>
+  </div>
+
+  <div v-if="showSecondModal">
+    <TheModal @close="toggleSecondModal">
+      <h2>Learn it now !!!</h2>
+      <p>Become a Vue Developer</p>
+    </TheModal>
+  </div>
+  <div class="buttons">
+    <button @click="toggleModal" class="btn-modal-one">open Modal 1</button>
+    <button @click="toggleSecondModal" class="btn-modal-two">
+      open Modal 2
+    </button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TheModal from './components/TheModal.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    TheModal,
+  },
+  data() {
+    return {
+      mainTitle: 'Vue Modal',
+      text: 'Welcome...',
+      showModal: false,
+      showSecondModal: false,
+    };
+  },
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal;
+    },
+    toggleSecondModal() {
+      this.showSecondModal = !this.showSecondModal;
+    },
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  font-family: Arial, Helvetica, sans-serif;
   text-align: center;
-  color: #2c3e50;
+  color: #333;
   margin-top: 60px;
+}
+
+body {
+  font-size: 62.5%;
+}
+
+h1 {
+  font-size: 4rem;
+}
+
+p {
+  font-size: 1.5rem;
+}
+
+.buttons {
+  padding: 2rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 2rem;
+}
+
+button {
+  padding: 1rem;
+  border-radius: 5px;
+  border: 1px solid #fff;
+  cursor: pointer;
+  color: #fff;
+  text-transform: uppercase;
+  font-weight: bold;
+  transition: all 0.5s;
+}
+
+.btn-modal-one {
+  background: #ff4b5c;
+}
+
+.btn-modal-one:hover {
+  background: #fff;
+  color: #ff4b5c;
+  border: 1px solid #ff4b5c;
+}
+
+.btn-modal-two {
+  background: #056674;
+}
+
+.btn-modal-two:hover {
+  background: #fff;
+  color: #056674;
+  border: 1px solid #056674;
 }
 </style>
---
+++ src/components/TheModal.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="backdrop" @click.self="closeModal">
+    <div class="modal">
+      <span class="toggle-btn" @click="closeModal">X</span>
+      <slot>default content</slot>
+      <div class="actions">
+        <slot name="links"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  emits: ['close'],
+  methods: {
+    closeModal() {
+      this.$emit('close');
+    },
+  },
+};
+</script>
+
+<style>
+.backdrop {
+  position: fixed;
+  top: 0;
+  background: rgba(0, 0, 0, 0.5);
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.modal {
+  position: relative;
+  width: 40rem;
+  padding: 2rem;
+  margin: 100px auto;
+  background: #fff;
+  border-radius: 10px;
+}
+
+.modal .toggle-btn {
+  position: absolute;
+  top: 2rem;
+  right: 2rem;
+  font-size: 1.5rem;
+  font-weight: bold;
+  cursor: pointer;
+}
+
+.modal h2 {
+  font-size: 3rem;
+  color: #056674;
+}
+
+.modal .actions {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 2rem;
+}
+
+.modal .actions a {
+  font-size: 1.5rem;
+  color: #056674;
+  text-decoration: none;
+  border: 1px solid #056674;
+  padding: 0.5rem 1rem;
+  border-radius: 5px;
+  transition: 0.5s;
+}
+
+.modal .actions a:hover {
+  background: #056674;
+  color: #fff;
+  border: 1px solid #056674;
+}
+</style>
